3c3c2a1728acca6f1b204e253ab6bfb39bad99e5
ANIEFIOK AKPAN <akp.ani@yahoo.com>
2020-10-09 16:08:05 +0100

    chore(update):updated file

--- bffbook/urls.py
+++ bffbook/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
+
     #path('profiles/',include('profiles.urls',namespace = 'profiles'))
     
 
